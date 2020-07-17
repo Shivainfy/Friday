89ef34db3d495096b30e86147c75bc7b9f0212ca
Shivainfy <shivabadiger7@gmail.com>
2020-07-17 10:50:43 -0230

    added covid project

---
+++ src/CovidPackage/Holiday.java
@@ -0,0 +1,25 @@
+package CovidPackage;
+
+import org.testng.annotations.Test;
+
+public class Holiday {
+
+	@Test
+	public void lenovo()
+	{
+		System.out.println("Lenovo computer");
+	}
+	
+	@Test
+	public void Dell()
+	{
+		System.out.println("DEll computer");
+	}
+	
+	@Test
+	public void HP()
+	{
+		System.out.println("HP computer");
+	}
+
+}
