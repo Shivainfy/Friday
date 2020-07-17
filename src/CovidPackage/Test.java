ca37f2ecee4372660c75256ddeef1fcda9c058b0
Shivainfy <shivabadiger7@gmail.com>
2020-07-17 11:04:16 -0230

    new variable added

--- src/CovidPackage/Test.java
+++ src/CovidPackage/Test.java
@@ -5,6 +5,9 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("added comments");
+		
+		String str ="shiva";
+		System.out.println("str");
 	}
 
 }
